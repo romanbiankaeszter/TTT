d1f71e20ba8c216665a2fb8fdf0412972e2ed1e8
Zoltan <mrnelf@gmail.com>
2019-10-16 12:55:56 +0200

    Win condition now works properly

--- TicTacToe.py
+++ TicTacToe.py
@@ -36,20 +36,7 @@ def init_board():
 
 
 
-    while game_running:
-
-        row1 = board[0][0] == board[0][1] == board[0][2] !="."
-        row2 = board[1][0] == board[1][1] == board[1][2] !="."
-        row3 = board[2][0] == board[2][1] == board[2][2] !="."
-
-        
-        column1 = board[0][0] == board[1][0] == board[2][0] !="."
-        column2 = board[0][1] == board[1][1] == board[2][1] !="."
-        column3 = board[0][2] == board[1][2] == board[2][2] !="."
-
-
-        across1 = board[0][0] == board[1][1] == board[2][2] !="."
-        across2 = board[0][2] == board[1][1] == board[2][0] !="."
+    while game_running is True:
         
         print_board(board, columns, rows)
         
@@ -57,13 +44,6 @@ def init_board():
 
         coor=input("Give a row and column: ")
 
-
-        # player changer
-
-        if current_player==p1:
-            current_player=p2
-        elif current_player ==p2:
-            current_player=p1
         
         # Marker
 
@@ -101,26 +81,49 @@ def init_board():
 
         print_board(board, columns, rows)
 
+        # player changer
+
+        if current_player==p1:
+            current_player=p2
+        elif current_player ==p2:
+            current_player=p1
         
+        #win condition
+
+        row1 = board[0][0] == board[0][1] == board[0][2] !="."
+        row2 = board[1][0] == board[1][1] == board[1][2] !="."
+        row3 = board[2][0] == board[2][1] == board[2][2] !="."
+
+        
+        column1 = board[0][0] == board[1][0] == board[2][0] !="."
+        column2 = board[0][1] == board[1][1] == board[2][1] !="."
+        column3 = board[0][2] == board[1][2] == board[2][2] !="."
+
+
+        across1 = board[0][0] == board[1][1] == board[2][2] !="."
+        across2 = board[0][2] == board[1][1] == board[2][0] !="."
+
         #game over
 
         if row1 or row2 or row3:
             game_running=False
+            
 
         if column1 or column2 or column3:
             game_running=False
+            
 
         if across1 or across2:
             game_running=False
+            
         
-        #exit
+    #exit
 
-        if game_running==False:
-            print("thx for playing!")
-            sys.exit(0)
+    if game_running==False:
+        print("thx for playing!")
+        sys.exit(0)
 
-
-    return board
+    
 
 
 def get_move(board, player):
@@ -182,6 +185,7 @@ def tictactoe_game(mode='HUMAN-HUMAN'):
 
     # use get_move(), mark(), has_won(), is_full(), and print_board() to create game logic
     print_board(board)
+
     row, col = get_move(board, 1)
     mark(board, 1, row, col)
 
